f4085e88f6b848cb844c0b8f716284a5cd4572aa
0GuLuca <145226452+0GuLuca@users.noreply.github.com>
2025-01-10 21:24:41 -0300

    TelaGanhouController.java

---
+++ TelaGanhouController.java
@@ -0,0 +1,23 @@
+package controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+public class TelaGanhouController implements Initializable {
+    @FXML
+    private Button btnAvancar;
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }   
+    
+    @FXML
+    private void handlebtnAvancarAction(ActionEvent event) {
+        
+    }
+}
